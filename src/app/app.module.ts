c5504a4204f12f7dedb5456d19934094cb81ba46
KenSv <sskkru@yahoo.com>
2019-03-15 17:23:06 +0300

    Added components, router module, guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { ServicesComponent } from './components/services/services.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { WebProjectsComponent } from './components/web-projects/web-projects.component';
+import { MobileAppsComponent } from './components/mobile-apps/mobile-apps.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServicesComponent,
+    ProjectsComponent,
+    GalleryComponent,
+    WebProjectsComponent,
+    MobileAppsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
